b982ce199cbd5940278f47f0f21ba17f4cc5d308
okaybenji <okaybenji@gmail.com>
2020-09-18 14:54:34 -0500

    change color of tv screen when player looks at it

--- game-disks/screen-creatures.js
+++ game-disks/screen-creatures.js
@@ -30,7 +30,11 @@ const screenCreatures = {
             `You feel something tug at you from the inside of your chest as you look at the screen. You feel an invisible hand pass though your skin as if you were clay and wrap its cold fingers around your lungs. You can’t breathe. 
 The screen is filled with so many colors, swirling around in maddening patterns with no rhyme or reason. With each second you stare, it becomes harder and harder to look away. You swear you can see another pair of eyes looking back at you.`,
             `The colors begin to pour out of the screen until they envelop you like a cocoon. You feel the colors press against you, wrapping themselves around you like a second skin. The colors are suddenly seared away by a blinding white light. Yet still you see those eyes, wide and unblinking, seared on to the back of your eyelids. The light dissipates, and the world begins to come back into focus. 
-You dare not close your eyes, for you know that if you do, it will be staring back at you.`]
+You dare not close your eyes, for you know that if you do, it will be staring back at you.`],
+          look() {
+            const tv = document.querySelector('.img');
+            tv.innerHTML = tv.innerHTML.replaceAll('--.__.--.__.--', '<span class="scanline">--.__.--.__.--</span>');
+          },
         },
       ],
     },
