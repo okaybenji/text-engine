6685a8bc5c995db747e772cc6593f23409edcc8e
okaybenji <okaybenji@gmail.com>
2020-10-02 17:29:12 -0500

    fix look methods; add ring

--- game-disks/une-belle-soiree.js
+++ game-disks/une-belle-soiree.js
@@ -2,15 +2,24 @@ const getNextDescription = (room) => room.descriptions.length ? room.description
 
 const uneBelleSoiree = {
   inventory: [{
-    name: 'hand-mirror',
-    desc: `You adjust your hair. Because of the boredom of provincial French life, what once felt like a duty has become a moment of excitement -- of diversion from your mother, your aunt, your brother, the occasional businessman visiting my father, none of whom you are given the opportunity to speak to. And strangely, in your excitement you also feel homesick and sad.`,
-    onLook: () => {
+    name: ['hand-mirror', 'mirror'],
+    desc: `You adjust your hair. Because of the boredom of provincial French life, what once felt like a duty has become a moment of excitement -- of diversion from your mother, your aunt, your brother. Rarely, the occasional businessmen visiting your father, none of whom you are given the opportunity to speak to. And strangely, in your excitement you also feel homesick and sad.`,
+    look: ({getRoom}) => {
+      const room = getRoom('start');
       room.desc = getNextDescription(room);
     },
     use: ({getRoom, println, item}) => {
       println(item.desc);
       const room = getRoom('start');
       room.desc = getNextDescription(room);
+    },
+  }, {
+    name: 'ring',
+    desc: `The ring was a gift from your father to your mother. You absentmindedly spin it on your finger and wonder, might you meet someone at the gathering? Someone who desires to adorn you with fine clothing and jewelry? Adornments you might lend your own daughters one day?`,
+    look: ({getRoom, item}) => {
+      const room = getRoom('start');
+      room.desc = getNextDescription(room);
+      item.desc = `It was a gift from your father to your mother.`;
     },
   }],
   roomId: 'start',
@@ -21,7 +30,9 @@ const uneBelleSoiree = {
       img: ``,
       descriptions: [
         `Underneath the beating of the hooves, you can hear the cicadas and bullfrogs of summer. There is a hand-mirror in your pocket.`,
-        `The rhythms of the ruts in the country mud begin to turn to the hum of paved roads as in the distance the haze of harvest begins to be illuminated by the lights of a far off estate, which is just now becoming encircled with a light fog.`,
+        `The rhythms of the carriage over ruts in the country mud begin to turn to the hum of paved roads as in the distance the haze of harvest begins to be illuminated by the lights of a far off estate, which is just now becoming encircled with a light fog.`,
+        `The light from the estate half-discloses the statuary that lines the entrance. From the roadside you hear the shouts of stablemen. Farther away, as quiet as a whisper, is a strange and exotic song.`,
+        `You can't make out the details, but the coachman is in some sort of conversation concerning the logistics of stabling the horses. Approaching the carriage is a servant of the House of Dauphin.`,
       ],
       items: [{
         name: ['invitation', 'letter'],
@@ -35,7 +46,7 @@ const uneBelleSoiree = {
           Mlle. Cassat is requested to attend the ball at Chateau de Dauphin on Tuesday the 1st of June at 8'00 PM.
           `;
 
-          item.onLook = () => {
+          item.look = () => {
             room.desc = getNextDescription(room);
           };
         },
