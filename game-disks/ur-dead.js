71eb9e6e7fd9c6233249277a9c0c0f707879ccfd
Benji Kay <okaybenji@gmail.com>
2020-11-01 01:30:38 -0500

    set up the conversation with the bearded skeleton

--- game-disks/ur-dead.js
+++ game-disks/ur-dead.js
@@ -99,16 +99,106 @@ There's a bearded skeleton by the sign. He seems to want to TALK.`,
       onTalk: () => println(`"Give it back," one of them says.`),
     },
     {
-      name: 'skeleton',
+      name: 'bearded skeleton',
       desc: `He appears to have something to say. Or at least, he keeps clearing his throat. Or, that is, if he had a throat... This is a confusing place.`,
       roomId: 'beach',
       topics: [
         {
-          option: 'TODO item',
-          line: `TODO: Give me some topics that lay out the player's goals in this game.`,
+          keyword: 'how',
+          option: `HOW did I get here?`,
+          line: `"You're dead," he says in a scratchy voice, "You must've gathered that much."`,
+          cb: () => {
+            // replace this topic
+            const skeleton = findCharacter('beard');
+            const topic = skeleton.topics.find(t => t.keyword === 'how');
+            topic.option = `HOW did I die?`;
+            topic.line = `He half-smiles and says, "That, you'll find, is the million-dollar question."
+            Mysterious.
+            "Well, one of them."
+            You don't know how you can tell he is smiling, but you CAN tell.`;
+            topic.cb = () => {
+              // remove this topic
+              disk.methods.removeTopic('beard', 'how');
+              // add another
+              const skeleton = findCharacter('beard');
+              skeleton.topics.push({
+                keyword: 'back',
+                option: `I don't want to die! How can I go BACK?`,
+                line: `He looks grim. "Now, I wouldn't get your hopes up on that matter. Almost nobody pulls it off. But it is my duty to tell you anyhow."`,
+                cb: () => {
+                  disk.methods.removeTopic('beard', 'back');
+                  const skeleton = findCharacter('beard');
+                  skeleton.topics = skeleton.topics.concat([{
+                    keyword: 'almost',
+                    option: `ALMOST no one?`,
+                    line: `"Well, yeah, there was Liam. Liam Hemsworth."`,
+                    cb: () => disk.methods.removeTopic('beard', 'almost'),
+                  }, {
+                    keyword: 'how',
+                    option: 'HOW then?',
+                    line: `"To get out of here, and back up there," he points up, "you need two pieces of information. One, your name. And two, as you asked before, HOW you died."
+
+                    It's at this moment that you realize you don't know your own name.
+
+                    He continues, "And there's a reason you're not likely to find this information: who would you ask? After all, we're all in the same boat, up the same river."
+
+                    He pauses to whistle a bar from COME SAIL AWAY.
+
+                    "In other words, if I don't know so much as my own name, how could I hope to tell you yours. You see?"
+
+                    This talking, bearded skeleton was starting to make some sense.
+
+                    "Anyway, I wouldn't worry too much about it. If you do happen across your name and cause of death, come back to me and I'll tell you where to go and who to talk to about it. But that's a whole other story, and as I said, it's not likely to come up! Just make yourself at home and start getting used to the place."
+                    `,
+                    cb: () => {
+                      disk.methods.removeTopic('beard', 'how');
+                      const skeleton = findCharacter('beard');
+                      skeleton.topics.push({
+                        keyword: 'home',
+                        option: `HOME? But where is home?`,
+                        line: `He smiles again, and holds out his hands as if to gesture: Everywhere.
+
+                        "Have a look around. You can venture wherever strikes your fancy. No one frets much over privacy or personal space. No one will accuse you of trespassing. You don't need to eat or sleep. And no real harm can come to you! Just explore and enjoy yourself.
+
+                        "Anyhow, I've said my spiel. I'll be here if you need me."
+
+                        You thank him and end the conversation.
+                        `,
+                        cb: () => {
+                          endConversation();
+                          disk.methods.removeTopic('beard', 'home');
+
+                          const skeleton = findCharacter('beard');
+                          skeleton.topics.push({
+                            keyword: 'what',
+                            option: `WHAT was I supposed to be doing again?`,
+                            line: `"If you're still trying to get out of here, come back once you've learned your NAME and HOW you died. Otherwise, just check things out and try to have a nice time."`,
+                          });
+                          skeleton.onTalk = () => {};
+                          skeleton.desc = `I wonder how he attaches that beard...`;
+
+                          // remove the last sentence of the description now that you've talked
+                          const room = getRoom(disk.roomId);
+                          room.desc = room.desc.replace(` He seems to want to TALK.`, '');
+                        },
+                      });
+                    },
+                  }]);
+                },
+              });
+            };
+
+            // add another topic
+            skeleton.topics.push({
+              keyword: 'skeleton',
+              option: `Will I become a SKELETON, too?`,
+              line: `"You already are one, as far as I can see!" he chuckles. "Down here, we can only see our own bodies. We all look like skeletons to one another. It's kind of nice, in a way. We don't have many beauty contests. Or anyway, our pageants don't hinge on natural good looks."`,
+              cb: () => disk.methods.removeTopic('beard', 'skeleton'),
+            });
+          },
         },
       ],
-      onTalk: () => println(`"You're dead," he says in a scratchy voice, "You must've gathered that much."`),
+      onTalk: () => println(`"I imagine," he begins, "you have some questions."`),
     },
   ],
   methods: {
@@ -124,6 +214,11 @@ There's a bearded skeleton by the sign. He seems to want to TALK.`,
       room.items.push(ball);
       const itemIndex = disk.inventory.findIndex(i => i === ball);
       disk.inventory.splice(itemIndex, 1);
+    },
+    removeTopic(characterName, keyword) {
+      const char = findCharacter(characterName);
+      const index = char.topics.findIndex(t => t.keyword === keyword);
+      char.topics.splice(index, 1);
     },
   },
 };
