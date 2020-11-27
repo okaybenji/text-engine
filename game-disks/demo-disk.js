5edb5146f28f1b8a78b2c8f966777dc1900937bf
Benji Kay <okaybenji@gmail.com>
2020-11-27 16:47:19 -0600

    update demo text

--- game-disks/demo-disk.js
+++ game-disks/demo-disk.js
@@ -6,22 +6,27 @@ const demoDisk = {
     {
       id: 'foyer',
       name: 'The Foyer',
-      desc:  `Welcome to the TEXT-ENGINE demo disk! This demo disk is a text adventure game designed to introduce you to the features available to you in text-engine. Using this engine, you can make a text game of your own.
+      desc:  `Welcome to the TEXT-ENGINE demo disk! This disk is a text adventure game designed to introduce you to the features available to you in text-engine. Using this engine, you can make a text game of your own.
 
       Type LOOK to have a look around.`,
       onLook() {
         const room = getRoom('foyer');
-        room.desc = `You are currently standing in the FOYER of the demo disk. Rooms form the foundation of the engine's design. At any given time, your player will be standing in one of the rooms your built for them on your game's disk. These can be literal rooms like the foyer you find yourself in now, or metaphorical rooms like "THE END OF TIME" or "PURGATORY".
+        room.desc = `You are currently standing in the FOYER. Rooms form the foundation of the engine's design. At any given time, your player will be standing in one of the rooms your built for them on your game's disk. These can be literal rooms like the foyer you find yourself in now, or metaphorical rooms like "THE END OF TIME" or "PURGATORY".
 
-        Each room you build should have a DESC (description). That's what you're reading now!`;
+        Each room you build should have a description. That's what you're reading now!
+
+        Rooms can also contain items. Type ITEMS to see a list of items in the FOYER. Or type HELP to see what else you can do!`;
       },
       items: [
         {
           name: 'style-changer',
+          desc: 'Type USE STYLE-CHANGER to try it out!',
+          isTakeable: true,
           onUse: () => {
             const currentStylesheet = document.getElementById('styles').getAttribute('href');
             const newName = currentStylesheet.includes('modern') ? 'retro' : 'modern';
-            println(`You changed the stylesheet to ${newName.toUpperCase()}.css`);
+            println(`You changed the stylesheet to ${newName.toUpperCase()}.css
+            Since text-engine is built with HTML, you can use Cascading Stylesheets (CSS) to make your game look however you want!`);
             selectStylesheet(`styles/${newName}.css`);
           }
         }
