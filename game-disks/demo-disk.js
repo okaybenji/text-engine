8ee73bbdbf02a244fe4e729b98cb1c4361ab84d3
okaybenji <okaybenji@gmail.com>
2022-07-07 11:10:24 -0500

    tweak rooms description on demo disk

--- game-disks/demo-disk.js
+++ game-disks/demo-disk.js
@@ -14,7 +14,7 @@ const demoDisk = () => ({
         const room = getRoom('foyer');
         room.desc = `You are currently standing in the foyer. There's a huge **MONSTERA** plant to your right, and a massive **WINDOW** to your left bathing the room in natural light. Both the **PLANT** and the **WINDOW** stretch to the ceiling, which must be at least 25 feet high.
 
-        ***Rooms** form the foundation of the engine's design. At any given time, your player will be standing in one of the rooms you built for them. These can be literal rooms like the foyer you find yourself in now, or metaphorical rooms like **The End of Time** or **Purgatory**.
+        ***Rooms** form the foundation of the engine's design. At any given time, your player will be standing in one of the rooms you built for them. These can be literal rooms like the foyer you find yourself in now, or metaphorical rooms like **The End of Time** or **A Dream**.
 
         Each room you create should have a description. (That's what you're reading now!)
 
